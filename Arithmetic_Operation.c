52f6278690cd072f301535d972d138feb23bc3bb
naveenkumar-2306 <naveenkumar.m@yantravision.com>
2023-12-20 14:44:58 +0530

    Arithmetic_Operation.c file created, main() Added
    	Arithmetic_Operation.c file is created and the main() funtion is
    added, to completed the project and the functionality to the files.

---
+++ Arithmetic_Operation.c
@@ -0,0 +1,34 @@
+#include <stdio.h> // Include the header file for the input and output
+
+
+int main()
+{
+int n1; // Initialize the variable "n1" for input1 
+int n2; // Initialize the variable "n2" for input2
+int task;
+int result;
+printf ("Enter the values of n1 and n2 by leaving one spacebar: ");
+scanf ("%d %d",&n1,&n2); // Scan the inputs and store it in variable address of n1, n2
+printf("Enter the operation need to perform");
+printf("1: Addition , 2: Subtraction , 3: multiplication :: ");
+scanf("%d",&task);
+switch (task)
+	{
+		case 1: 
+	    		result = addition (&n1,&n2); //Send the value of n1 and n2 as function addition
+	    		printf("The result of operation : %d ",result);
+	    		break;
+		case 2:
+		   	result = subtraction (&n1,&n2); //Send the value of n1 and n2 as function subtraction
+		   	printf("The result of operation : %d ",result);
+			break;
+		case 3: 
+		    	result = multiplication (&n1,&n2); //Send the value of n1 and n2 as function multiply
+		    	printf("The result of operation : %d ",result);
+			break;
+		default: 
+			printf("Invalid task selected");
+	}
+	
+	
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Arithmetic-Operation
+
+The program file Arithmetic_Operation.c for the Arithmetic operation such as Addition, Subtraction, Multiplication
+
+Step 1: Run the Program using using the terminal after change the file into an execution file
+Step 2: Enter the value of the numbers n1 and n2 both positive and negative
+Step 3: Enter the operation to be performed 1: Addition, 2: Subtraction, 3: Multiplication
+Step 4: You find the result of the operation.
